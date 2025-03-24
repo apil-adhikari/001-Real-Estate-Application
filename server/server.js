0b455b83b62a19b934b493e33b2fe62bcb160f8c
Apil Adhikari <apiladhikari.social@gmail.com>
2025-03-24 21:38:56 +0545

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+import app from "./src/app.js"; // Importing Express application
+
+const port = process.env.PORT || 8001;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
