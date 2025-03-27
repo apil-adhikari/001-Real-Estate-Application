43412db75e77aa4b2522db3c67d533ff373e5901
Apil Adhikari <apiladhikari.social@gmail.com>
2025-03-27 20:33:05 +0545

    Added map using react-leaflet

--- client/src/routes/listPage/ListPage.jsx
+++ client/src/routes/listPage/ListPage.jsx
@@ -4,6 +4,7 @@ import { listData } from "../../lib/dummyData";
 import React from "react";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
 
 const ListPage = () => {
   const data = listData;
@@ -18,7 +19,9 @@ const ListPage = () => {
           ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map />
+      </div>
     </div>
   );
 };
