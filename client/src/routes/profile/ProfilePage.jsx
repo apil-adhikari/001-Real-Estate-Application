674357165fbc9f0db7df0c2aafe69412c2a34c32
Apil Adhikari <apiladhikari.social@gmail.com>
2025-03-31 19:13:08 +0545

    Aded user detail section and made responsive for medium and smaller screen

--- client/src/routes/profile/ProfilePage.jsx
+++ client/src/routes/profile/ProfilePage.jsx
@@ -3,7 +3,55 @@ import "./profilePage.scss";
 import React from "react";
 
 const ProfilePage = () => {
-  return <div className="profilePage">ProfilePage</div>;
+  return (
+    <div className="profilePage">
+      <div className="leftContainer">
+        {/* User Profile */}
+        <div className="userProfileContainer">
+          <div className="containerHeader">
+            <h1>User Information</h1>
+            <button>Update</button>
+          </div>
+
+          <div className="userDetails">
+            <div className="nameEmailContainer">
+              <div className="userName">
+                <sapn>Name: </sapn>
+                Jenny Martin
+              </div>
+              <div className="userEmail">
+                <span>Email: </span>jennymartin@gmail.com
+              </div>
+            </div>
+
+            <img
+              className="userAvatar"
+              src="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt="Profile Picture"
+            />
+          </div>
+        </div>
+
+        {/* My Listings */}
+        <div className="userListingsContainer">
+          <div className="containerHeader">
+            <h1>My Listings</h1>
+            <button>Add Post</button>
+          </div>
+          <div className="userAddedPost"></div>
+        </div>
+
+        {/* Saved */}
+        <div className="userSavedContainer">
+          <div className="containerHeader">
+            <h1>Saved Posts</h1>
+          </div>
+          <div className="userSavedCards"></div>
+        </div>
+      </div>
+      <div className="rightContainer">right</div>
+    </div>
+  );
 };
 
 export default ProfilePage;
