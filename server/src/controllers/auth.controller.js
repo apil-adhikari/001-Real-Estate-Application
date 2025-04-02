81cde224cbac7cc4b03c57db15f83548c145125c
Apil Adhikari <apiladhikari.social@gmail.com>
2025-04-02 19:50:16 +0545

    Add register user controller to register a user by taking the inputs - Use @prisma/client to use database queries - Check for empty fields, already use email address and hash password before creating user - Use JSend specification to send response

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,27 +1,65 @@
 import bcrypt from "bcrypt";
+import prisma from "../../lib/prisma.js";
 
 /**Register a user */
 export const register = async (req, res) => {
-  const { username, email, password } = req.body;
-  console.log("req.body: ", req.body);
+  try {
+    const { username, email, password } = req.body;
 
-  // Check if the fields are filled
-  if (!username || !email || !password) {
-    return res.status(400).json({
-      status: "fail",
+    // Check if the fields are filled
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        data: {
+          message: "Please enter all fields!",
+        },
+      });
+    }
+
+    // Check if the user already exists with that  email address
+    const userExists = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({
+        status: "fail",
+        data: {
+          message:
+            "User already exists with the email address. Please use use different email or login!",
+        },
+      });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 12);
+    console.log("Hashed Password", hashedPassword);
+
+    // Create a new user and save it to db
+    const user = await prisma.user.create({
       data: {
-        message: "Please enter all fields!",
+        username,
+        email,
+        password: hashedPassword,
       },
     });
+    console.log(user);
+
+    // Send response
+    res.status(201).json({
+      status: "success",
+      data: {
+        message: "User created successfully",
+        user,
+      },
+    });
+  } catch (error) {
+    console.log("Error in register controller: ", error);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
   }
-
-  // Check if the user already exists with that  email address
-
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 12);
-  console.log("Hashed Password", hashedPassword);
-
-  // Create a new user and save it to db
 };
 
 /**Login */
