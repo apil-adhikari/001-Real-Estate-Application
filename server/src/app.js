3e671a4af345c3bb928ce8c198e6441046980f78
Apil Adhikari <apiladhikari.social@gmail.com>
2025-04-03 11:34:19 +0545

    feat(auth): implement login controller with JWT authentication - Vallidate email and password fields for empitness and correctness. - Generate JWT token upon successful login. - Send token securly via HTTP-Only cookie using eq.cookie(). - Utilize jsonwebtoken for token creation and cookie-parser for handling cookies.

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import postRouter from "./routes/post.routes.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
@@ -7,6 +9,8 @@ const app = express();
 
 /** This is a built-in middleware function in Express. It parses incoming requests with JSON payloads. It is based on body-parser */
 app.use(express.json());
+/**Parse Cookie header and populate req.cookies with an object keyed by the cookie names. */
+app.use(cookieParser());
 
 // Mounting common routes using middleware
 app.use("/api/v1/posts", postRouter);
--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import prisma from "../../lib/prisma.js";
 
 /**Register a user */
@@ -63,8 +65,76 @@ export const register = async (req, res) => {
 };
 
 /**Login */
-export const login = (req, res) => {
-  // db operations
+export const login = async (req, res) => {
+  try {
+    // Take all required inputs and destructure
+    const { email, password } = req.body;
+    console.log("Authorization Header: ", req.headers.authorization);
+
+    // Check if requied fields are filled
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        data: {
+          missingFields: ["email", "password"],
+          message: "Please enter all fields!",
+        },
+      });
+    }
+
+    // Check if the user exists for the entered email
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (!existingUser) {
+      return res.status(401).json({
+        status: "fail",
+        data: {
+          message: "Invalid credintials!",
+        },
+      });
+    }
+
+    // Check if the password is correct
+    // Check if the entered password matches with the password stored in db for the found (existing) user
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        status: "fail",
+        data: {
+          message: "Invalid credintials!",
+        },
+      });
+    }
+
+    // Generate Token
+    // res.setHeader("Set-Cookie", "text=" + "myvalue"); // Manually sending cookies header
+
+    const token = jwt.sign(
+      { id: existingUser.id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: process.env.JWT_SECRET_EXPIRES_IN }
+    );
+
+    // Define the cookie options
+    const cookieOptions = {
+      maxAge: 3600000, //Cookie will expire in 1 hour(360000 miliseconds)
+      httpOnly: true, // Makes the cookie inaccessible to the client-side JavaScritp
+      secure: true, //Ensure cookie are sent over HTTPS
+    };
+
+    res.cookie("token", token, cookieOptions).json({
+      status: "success",
+    });
+  } catch (error) {
+    console.log("Error in login controller: ", error);
+    res.status(500).json({
+      status: "error",
+      mesasge: "Something went wrong!",
+    });
+  }
 };
 
 /**Logout */
