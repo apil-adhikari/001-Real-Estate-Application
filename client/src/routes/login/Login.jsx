608e48a15774934e4f52f1774cbda7df930d1020
Apil Adhikari <apiladhikari.social@gmail.com>
2025-04-03 13:22:29 +0545

    feat(ui): create login page with input fields and basic validation - Designed login form with email and password fields. - Added basic required validation. - Styled the form using SCSS for better UI/UX.

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,8 +1,60 @@
+import { Link } from "react-router-dom";
 import "./login.scss";
-import React from "react";
+import { MdEmail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { RiTextBlock } from "react-icons/ri";
+import { IoLockClosed, IoLockOpen, IoMail, IoPerson } from "react-icons/io5";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form>
+          <h1>Login into your Account</h1>
+          <div className="labelContainer">
+            <label htmlFor="username">
+              <IoPerson className="icon" />
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className="labelContainer">
+            <label htmlFor="email">
+              <IoMail className="icon" />
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="labelContainer">
+            <label htmlFor="password">
+              <IoLockClosed className="icon" />
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button>Login</button>
+          <span>
+            Don't have an account? <Link to="/register"> Register</Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
